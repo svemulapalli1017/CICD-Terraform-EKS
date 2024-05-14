84a47b5efadfe782d003d74e7c0d4de8178db140
svemulapalli1017 <svemulapalli1017@my.msutexas.edu>
2024-05-14 14:55:38 -0400

    Fixed duplicate data resource names in Terraform configuration

--- EKS/kubernetes.tf
+++ EKS/kubernetes.tf
@@ -1,5 +1,5 @@
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster_data.endpoint
   token                  = data.aws_eks_cluster_auth.cluster_auth_data.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster_data.certificate_authority.0.data)
 }
