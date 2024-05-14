a7ac678f2317e5d764f9ac67aa009d4dfb41a33f
svemulapalli1017 <svemulapalli1017@my.msutexas.edu>
2024-05-14 03:08:15 -0400

    Fixed duplicate data resource names in Terraform configuration

--- EKS/data.tf
+++ EKS/data.tf
@@ -1,10 +1,10 @@
 data "aws_availability_zones" "azs" {}
 
-data "aws_eks_cluster" "cluster" {
+data "aws_eks_cluster" "cluster_data" {
   name = module.eks.cluster_id
 }
 
-data "aws_eks_cluster_auth" "cluster" {
+data "aws_eks_cluster_auth" "cluster_auth_data" {
   name = module.eks.cluster_name
   depends_on = [ module.eks]
 }
