ee7b6f88f22033dadb58732e41f1f08793bd1808
svemulapalli1017 <svemulapalli1017@my.msutexas.edu>
2024-05-14 01:58:40 -0400

    added kubernetes file

--- EKS/main.tf
+++ EKS/main.tf
@@ -52,3 +52,12 @@ module "eks" {
     Terraform   = "true"
   }
 }
+
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
